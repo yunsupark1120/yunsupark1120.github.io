bd6f738223372e0a071e6d1b6266040dccca00ef
yunsupark1120 <yunsupark1120@gmail.com>
2024-02-08 15:53:07 +0900

    chapter 1 design

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707367719',resources: [
+    cacheName: 'chirpy-1707375104',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
