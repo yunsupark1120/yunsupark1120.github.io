c5682c55fdc8ff2228432249ae05a025a89ae6c6
yunsupark1120 <yunsupark1120@gmail.com>
2024-02-09 13:56:46 +0900

    image

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707454560',resources: [
+    cacheName: 'chirpy-1707454596',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
