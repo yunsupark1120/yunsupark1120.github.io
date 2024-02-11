10edf091079126b26cde0cbc6a36f276c2b8ea78
yunsupark1120 <yunsupark1120@gmail.com>
2024-02-11 21:31:52 +0900

    two names

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707653509',resources: [
+    cacheName: 'chirpy-1707654182',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
