d80c0f4385dd7e025f66e34f9c7908fa5b41153e
yunsupark1120 <yunsupark1120@gmail.com>
2024-02-11 21:12:37 +0900

    Euler's Formula post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707482061',resources: [
+    cacheName: 'chirpy-1707653509',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
