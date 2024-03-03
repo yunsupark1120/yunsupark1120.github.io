751ef2df219f6554b5ad64fc7c0a3906b05ef980
yunsupark1120 <yunsupark1120@gmail.com>
2024-03-04 00:59:59 +0900

    Python TUtorial Chapter 2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708061275',resources: [
+    cacheName: 'chirpy-1709481563',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
